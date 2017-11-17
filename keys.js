05d9335a7c2c4b6fd714e4b001e768841e5a6871
realonlytomd <mclaughlintomd@gmail.com>
2017-11-17 17:45:57 -0600

    first commit of new files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'PbgG6jGqcLsAvJvI4nSetQsna',
+  consumer_secret: 'Wsb6eJcjC4k7OksRf88BtWVQx5nMtLuH5vA70y5lcJz5K4Xp5x',
+  access_token_key: '20564246-wWaYkWHJWiWfZzQLQsWqksalr2WTNjirMnEbnzyln',
+  access_token_secret: 'liOYSr3AJPuqRPCOyrUdW0zgdq1cRIWqPQ0iiFrXTLCxg',
+}
+
+module.exports = twitterKeys;
