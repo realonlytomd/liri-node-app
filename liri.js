37a6ba1106d70e0ef5544c43c6762b6614dbebd0
realonlytomd <mclaughlintomd@gmail.com>
2017-11-20 15:51:34 -0600

    while working on omdb portion, after npm init

--- liri.js
+++ liri.js
@@ -1,3 +1,34 @@
+var fs = require("fs");
+var request = require("request");
+
 console.log(process.argv);
 
 var twitterInfo = require("./keys.js");
+
+var action = process.argv[2];
+var userInput = process.argv[3];
+
+switch (action) {
+  case "my-tweets":
+    tweets();
+    break;
+
+  case "spotify-this-song":
+    spotify();
+    break;
+
+  case "movie-this":
+    movie();
+    break;
+
+  case "do-what-it-says":
+    dowhat();
+    break;
+}
+
+function movie() {
+
+	movieName = userInput;
+	console.log(movieName);
+
+}
